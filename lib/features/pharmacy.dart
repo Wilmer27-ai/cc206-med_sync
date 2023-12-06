3e61ec398e7c77ddcadb63e9f35dab9a44e4a197
Wilmer Suelo <suelowilmer27@gmail.com>
2023-12-06 08:26:32 +0800

    make some changes for scrolling

--- lib/features/pharmacy.dart
+++ lib/features/pharmacy.dart
@@ -61,127 +61,152 @@ class PharmacyPage extends StatelessWidget {
           // Remove the cart icon from the title page
         ],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            // Search bar
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Search for drugs...',
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(10.0),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  // Cart icon on the right side of the search bar with border
-                  Container(
+    body: Container(
+  constraints: BoxConstraints(
+    minHeight: MediaQuery.of(context).size.height,
+  ),
+  padding: const EdgeInsets.all(8.0),
+  child: CustomScrollView(
+    slivers: [
+      SliverList(
+        delegate: SliverChildListDelegate([
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
                     decoration: BoxDecoration(
                       color: Colors.grey[300],
                       borderRadius: BorderRadius.circular(25.0),
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Icon(
-                        Icons.shopping_cart,
-                        size: 20,
-                        color: Colors.black,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Search for drugs...',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(10.0),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'All Products',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                  color: Colors.black,
                 ),
+                const SizedBox(width: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Icon(
+                      Icons.shopping_cart,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'All Products',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+                color: Colors.black,
               ),
             ),
-            
-
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-
-                  },
-                  child: ClipRect(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: index == 0
+          ),
+        ]),
+      ),
+      SliverGrid(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+        ),
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: ClipRect(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: index == 0
+                      ? Image.asset(
+                          'lib/features/assets/ph1.png',
+                          fit: BoxFit.fill,
+                        )
+                      : index == 1
                           ? Image.asset(
-                              'lib/features/assets/ph1.png',
+                              'lib/features/assets/ph2.png',
                               fit: BoxFit.fill,
                             )
-                          : index == 1
+                          : index == 2
+                              ? Image.asset(
+                                  'lib/features/assets/ph3.png',
+                                  fit: BoxFit.fill,
+                                )
+                              : index == 3
                                   ? Image.asset(
-                                      'lib/features/assets/ph2.png',
+                                      'lib/features/assets/ph4.png',
                                       fit: BoxFit.fill,
                                     )
-                              : index == 2
-                                  ? Image.asset(
-                                      'lib/features/assets/ph3.png',
-                                      fit: BoxFit.fill,
-                                    )
-                                  : index == 3
+                                  : index == 4
                                       ? Image.asset(
-                                          'lib/features/assets/ph4.png',
+                                          'lib/features/assets/ph1.png',
                                           fit: BoxFit.fill,
                                         )
-                                      : index == 4
+                                      : index == 5
                                           ? Image.asset(
                                               'lib/features/assets/ph1.png',
                                               fit: BoxFit.fill,
                                             )
-                                          : Image.asset(
-                                              'lib/features/assets/ph2.png',
-                                              fit: BoxFit.fill,
-                                            ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
+                                          : index == 6
+                                              ? Image.asset(
+                                                  'lib/features/assets/ph3.png',
+                                                  fit: BoxFit.fill,
+                                                )
+                                              : index == 7
+                                                  ? Image.asset(
+                                                      'lib/features/assets/ph1.png',
+                                                      fit: BoxFit.fill,
+                                                    )
+                                                  : index == 8
+                                                      ? Image.asset(
+                                                          'lib/features/assets/ph2.png',
+                                                          fit: BoxFit.fill,
+                                                        )
+                                                      : index == 9
+                                                          ? Image.asset(
+                                                              'lib/features/assets/ph4.png',
+                                                              fit: BoxFit.fill,
+                                                            )
+                                                          : Image.asset(
+                                                              'lib/features/assets/ph2.png',
+                                                              fit: BoxFit.fill,
+                                                            ),
+                ),
+              ),
+            );
+          },
+          childCount: 10,
         ),
       ),
+    ],
+  ),
+),
     );
   }
-}
+} 
